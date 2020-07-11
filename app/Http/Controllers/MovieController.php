8c136e0c71d1b77312303540976bf88d54b64061
samoraa <0x0978@protonmail.com>
2020-07-11 20:12:50 +0800

    feat: add caching feature

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,33 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use App\ViewModels\MovieViewModel;
 use App\ViewModels\MoviesViewModel;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class MovieController extends Controller
 {
     public function index()
     {
         // Get popular movies
-        $popularMovies = Http::withToken(config('services.tmdb.token'))
-                            ->get('https://api.themoviedb.org/3/movie/popular')
-                            ->json()['results'];
+        $popularMovies = Cache::remember('popular-movies', Carbon::parse('10 minutes'), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                        ->get('https://api.themoviedb.org/3/movie/popular')
+                        ->json()['results'];
+        });
 
         // Get now playing movies
-        $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
-                            ->get('https://api.themoviedb.org/3/movie/now_playing')
-                            ->json()['results'];
+        $nowPlayingMovies = Cache::remember('now-playing-movies', Carbon::parse('10 minutes'), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                        ->get('https://api.themoviedb.org/3/movie/now_playing')
+                        ->json()['results'];
+        });
 
         // Get all genre
-        $genreArray = Http::withToken(config('services.tmdb.token'))
-                            ->get('https://api.themoviedb.org/3/genre/movie/list')
-                            ->json()['genres'];
+        $genres = Cache::remember('genres', Carbon::parse('10 minutes'), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                        ->get('https://api.themoviedb.org/3/genre/movie/list')
+                        ->json()['genres'];
+        });
 
         $viewModel = new MoviesViewModel(
             $popularMovies,
             $nowPlayingMovies,
-            $genreArray,
+            $genres,
         );
 
         return view('movie.index', $viewModel);
@@ -39,8 +47,8 @@ class MovieController extends Controller
     {
         // Get a movie by id
         $movie = Http::withToken(config('services.tmdb.token'))
-                        ->get('https://api.themoviedb.org/3/movie/' . $id . '?append_to_response=videos,images')
-                        ->json();
+                    ->get('https://api.themoviedb.org/3/movie/' . $id . '?append_to_response=videos,images')
+                    ->json();
 
         $viewModel = new MovieViewModel($movie);
 
